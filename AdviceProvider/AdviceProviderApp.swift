3871ba82abc5d7c874cb428b92d8434b710117c5
zaid ragab <zragab@lcs.on.ca>
2023-04-20 08:39:18 -0400

    Initial Commit

---
+++ AdviceProvider/AdviceProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdviceProviderApp.swift
+//  AdviceProvider
+//
+//  Created by Zaid Ragab on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct AdviceProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
